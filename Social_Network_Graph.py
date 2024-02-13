0f0f324ffbd1f2cd2d5a42aaa9a6ee2f483224e9
reagan-pius <goingreaganp@gmail.com>
2024-02-13 19:39:45 +0000

    Added a method to find friends of a member

--- Social_Network_Graph.py
+++ Social_Network_Graph.py
@@ -15,3 +15,22 @@ class Graph:
             #Create a new member with the kwargs
             self.relationships[member] = []
             #Create relationships for the member
+
+    def find_friends(self, member: str):
+        """
+        This method finds all the friends of a particular member.
+
+        Args:
+        - member: this is the Name of the member
+
+        Returns:
+        - List of all the member's friends
+
+        Usage:
+        print("Friends of Alice:", social_network.find_friends("Alice"))
+        """
+        if member in self.members:
+            return self.relationships[member]
+        else:
+            return []
+    
