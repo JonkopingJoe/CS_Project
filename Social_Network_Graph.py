5da0c0fc2ed8644a454c9dd7775ce46183eaae7c
Joe <144815201+JonkopingJoe@users.noreply.github.com>
2024-02-13 13:49:14 +0000

    Update Social_Network_Graph.py

--- Social_Network_Graph.py
+++ Social_Network_Graph.py
@@ -1,1 +1,17 @@
 #First project
+#Social Network Graph
+
+class Graph:
+    def __init__(self):
+        self.members = {}
+        #Create a dictionary to store members as Nodes
+        self.relationships = {}
+        #Create a dictionary to store relationships as Edges
+    
+    def add_member(self, member: str, **kwargs):
+        if member not in self.members:
+        #Check if the member exists
+            self.members[member] = kwargs
+            #Create a new member with the kwargs
+            self.relationships[member] = []
+            #Create relationships for the member
