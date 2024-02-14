a3c148576c5d0313114144aa56e4d4dabd22ab3f
iiicartoonn <neens.r@gmail.com>
2024-02-14 13:22:50 +0000

    added remove_edge method

--- Network Connectivity.py
+++ Network Connectivity.py
@@ -21,6 +21,13 @@ class Graph:
         else:
             print('Edge already exists between the vertices.')
 
+    def remove_edge(self, vertex1, vertex2):
+        if vertex1 in self.vertices[vertex2] and vertex2 in self.vertices[vertex1]:
+            self.vertices[vertex1].remove(vertex2)
+            self.vertices[vertex2].remove(vertex1)
+        else:
+            print('Edge does not exist between the vertices.')
+
 
 
 
