b30a6e2414d820cdaf9d490b6ec5c01c054c6a97
iiicartoonn <neens.r@gmail.com>
2024-02-14 13:20:02 +0000

    added add_edge methode

--- Network Connectivity.py
+++ Network Connectivity.py
@@ -13,3 +13,14 @@ class Graph:
         """
         if vertex not in self.vertices:
             self.vertices[vertex] = set()
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 not in self.vertices[vertex2] and vertex2 not in self.vertices[vertex1]:
+            self.vertices[vertex1].append(vertex2)
+            self.vertices[vertex2].append(vertex1)
+        else:
+            print('Edge already exists between the vertices.')
+
+
+
+
