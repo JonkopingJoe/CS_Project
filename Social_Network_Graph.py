567c45c0650a9fc694970e5a49f1249a6b56a40d
iiicartoonn <neens.r@gmail.com>
2024-02-14 22:36:01 +0000

    changed class Node to Member for better readability as well as to comply with the worksheet

--- Social_Network_Graph.py
+++ Social_Network_Graph.py
@@ -1,23 +1,23 @@
-class Node:
+class Member:
     def __init__(self, name: str, age: int, location: str):
         """
-        Initialize a Node object with a name, age, and location.
+        Initialize a Member object with a name, age, and location.
 
         Args:
             name (str): The name of the person.
             age (int): The age of the person.
             location (str): The location of the person.
         Usage:
-            spongeBob = Node("SpongeBob SquarePants",35,PeanappleHouse)
+            spongeBob = Member("SpongeBob SquarePants",35,PeanappleHouse)
         """
         self.name = name
         self.age = age
         self.location = location
-        self.friends_list = [] # Holds nodes in the graph class
+        self.friends_list = [] # Holds members in the graph class
 
     def add_friend(self, new_friend) -> None:  # Makes new edge
         """
-        Add a new friend to the node's friends list, to abstract an edge
+        Add a new friend to the member's friends list, to abstract an edge
 
         Args:
             new_friend (Node): The node representing the new friend to connect.
@@ -26,7 +26,7 @@ class Node:
             None
 
         Usage:
-            spongeBob.add_firend(patrick)
+            spongeBob.add_friend(patrick)
         """
         if new_friend not in self.friends_list:
             self.friends_list.append(new_friend)
@@ -39,33 +39,33 @@ class Graph:
     Initialize an empty graph.
     """
     def __init__(self):
-        self.set_of_nodes = set()
+        self.set_of_members = set()
 
-    def add_node(self, node: Node) -> None:
+    def add_member(self, member: Member) -> None:
         """
         Add a node to the graph.
 
         Args:
-            node (Node): The node to be added to the graph, an instance of class Node.
+            member (Member): The node to be added to the graph, an instance of class Member.
 
         Returns:
             None
 
         Usage:
-            bikiniBottom.add_node(spongeBob)
+            bikiniBottom.add_member(spongeBob)
         """
-        if node not in self.set_of_nodes:
-            self.set_of_nodes.add(node)
+        if member not in self.set_of_members:
+            self.set_of_members.add(member)
 
         return None
 
-    def add_relations(self, person1: Node, person2: Node) -> None:
+    def add_relations(self, person1: Member, person2: Member) -> None:
         """
-        Add a relation between two nodes in the graph, representing an edge.
+        Add a relation between two members in the graph, representing an edge.
 
         Args:
-            person1 (Node): The first node, an instance of class Node.
-            person2 (Node): The second node, an instance of class Node.
+            person1 (Node): The first node, an instance of class Member.
+            person2 (Node): The second node, an instance of class Member.
 
         Returns:
             None
@@ -73,7 +73,7 @@ class Graph:
         Usage:
             bikinibottom.add_relations(spongeBob, patrick)
         """
-        if person1 in self.set_of_nodes and person2 in self.set_of_nodes:
+        if person1 in self.set_of_members and person2 in self.set_of_members:
             person1.add_friend(person2)
             person2.add_friend(person1)
 
@@ -92,7 +92,7 @@ class Graph:
         Usage:
             bikiniBottom.get_friends(spongeBob)
         """
-        for person in self.set_of_nodes:
+        for person in self.set_of_members:
             if person.name == name:
                 return person.friends_list
 
