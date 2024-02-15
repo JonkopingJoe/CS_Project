bf421af9ded902f4f7c577ef4289e24faad6fd9a
reagan-pius <goingreaganp@gmail.com>
2024-02-15 17:22:50 +0000

    Created Graph Class for Network Connections with a global function to see if the whole network is connected

--- Network Connectivity.py
+++ Network Connectivity.py
@@ -2,26 +2,60 @@
 
 class Graph:
     def __init__(self):
-        """
-        Initializes an empty graph.
-        """
-        self.vertices = {}
-
+        self.adjacency_matrix = {}
+        
+    def add_vertex(self, vertex):
+        if vertex not in self.adjacency_matrix:
+            self.adjacency_matrix[vertex] = []
+    
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_matrix and vertex2 in self.adjacency_matrix:
+            self.adjacency_matrix[vertex1].append(vertex2)
+            self.adjacency_matrix[vertex2].append(vertex1)
+    
+    def get_adjacency_matrix(self):
+        return self.adjacency_matrix
     
 
-    def add_edge(self, vertex1, vertex2):
-        if vertex1 not in self.vertices[vertex2] and vertex2 not in self.vertices[vertex1]:
-            self.vertices[vertex1].append(vertex2)
-            self.vertices[vertex2].append(vertex1)
-        else:
-            print('Edge already exists between the vertices.')
+def is_network_connected(graph) -> bool:
+    
+    visited = set()
+    stack = [next(iter(graph.adjacency_matrix))]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend([v for v in graph.adjacency_matrix[vertex] if v not in visited])
+    
+    return len(visited) == len(graph.adjacency_matrix)
 
-    def remove_edge(self, vertex1, vertex2):
-        if vertex1 in self.vertices[vertex2] and vertex2 in self.vertices[vertex1]:
-            self.vertices[vertex1].remove(vertex2)
-            self.vertices[vertex2].remove(vertex1)
-        else:
-            print('Edge does not exist between the vertices.')
+def connection_of_node(self, vertex) -> list: 
+    keys_list = list(self.adjacency_matrix.keys()).sort()
+    connections_of_vertex = self.adjacency_matrix[vertex].sort()
+    
+    connection_row = []
+
+    for key in keys_list: 
+        if key in connections_of_vertex: 
+            connection_row.append(1)
+        else: 
+            connection_row.append(0)
+    
+    return connection_row
+
+
+
+
+def get_adj_matrix(self) -> list:
+    adj_matrix = []
+    vertices_list = list(self.adjacency_matrix.keys()).sort()
+
+    for vertex in vertices_list: 
+        adj_matrix.append(self.connection_of_node(vertex))
+
+    return adj_matrix
+
+
 
 
 
