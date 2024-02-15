925f709669788059aabb34028ba14cb5c7a7a7c2
IMKSD <88252358+IMKSD@users.noreply.github.com>
2024-02-15 17:54:26 +0000

    slight modifications to the adj_matrix method and class in general

--- Network Connectivity.py
+++ Network Connectivity.py
@@ -13,6 +13,29 @@ class Graph:
             self.adjacency_matrix[vertex1].append(vertex2)
             self.adjacency_matrix[vertex2].append(vertex1)
 
+    def connection_of_node(self, vertex) -> list: 
+        keys_list = sorted(list(self.adjacency_matrix.keys()))
+        connections_of_vertex = sorted(self.adjacency_matrix[vertex])
+    
+        connection_row = []
+
+        for key in keys_list: 
+            if key in connections_of_vertex: 
+                connection_row.append(1)
+            else: 
+                connection_row.append(0)
+        
+        return connection_row
+
+    def get_adj_matrix(self) -> list:
+            adj_matrix = []
+            vertices_list = sorted(list(self.adjacency_matrix.keys()))
+        
+            for vertex in vertices_list: 
+                adj_matrix.append(self.connection_of_node(vertex))
+        
+            return adj_matrix
+
     
 
 def is_network_connected(graph) -> bool:
@@ -26,32 +49,6 @@ def is_network_connected(graph) -> bool:
             stack.extend([v for v in graph.adjacency_matrix[vertex] if v not in visited])
     
     return len(visited) == len(graph.adjacency_matrix)
-
-def connection_of_node(self, vertex) -> list: 
-    keys_list = list(self.adjacency_matrix.keys()).sort()
-    connections_of_vertex = self.adjacency_matrix[vertex].sort()
-    
-    connection_row = []
-
-    for key in keys_list: 
-        if key in connections_of_vertex: 
-            connection_row.append(1)
-        else: 
-            connection_row.append(0)
-    
-    return connection_row
-
-
-
-
-def get_adj_matrix(self) -> list:
-    adj_matrix = []
-    vertices_list = list(self.adjacency_matrix.keys()).sort()
-
-    for vertex in vertices_list: 
-        adj_matrix.append(self.connection_of_node(vertex))
-
-    return adj_matrix
 
 
 
