63443c2a1d442ffc72106b20ff792ac0d5e85d26
reagan-pius <goingreaganp@gmail.com>
2024-03-01 02:48:33 +0000

    Edits

--- Word_Ladder_Game.py
+++ Word_Ladder_Game.py
@@ -1,3 +1,30 @@
+class WordNode:
+    def __init__(self, word:str) -> None:
+        self.word = word
+        self.l1 = word[0]
+        self.l2 = word[1]
+        self.l3 = word[2]
+        self.neighbors = []
+
+
+    def get_neighbors(self):
+        return self.neighbors
+    
+class Graph:
+    def __init__(self) -> None:
+        self.set_of_wordnodes = set()
+
+    def add_word(self, new_wordnode) -> None:
+        self.set_of_wordnodes.add(new_word)
+        for wordnode in self.set_of_wordnodes:
+            if wordnode.l1 == new_wordnode.l1 or wordnode.l2 == new_wordnode.l2 or wordnode.l3 == new_wordnode.l3:
+                wordnode.add_neighbor(new_wordnode)
+                new_wordnode.add_neighbor(wordnode)
+    
+    # def find_ladder(self, start, end):
+
+
+
 #  Function to load the words we need from the database
 def load_all_words(file_name: str) -> list:
     with open("words_alpha.txt", 'r') as f:
@@ -58,28 +85,3 @@ def word_ladder(word_start, word_end, words) -> None:
                 visited.add(neighbour)
                 queue.append((neighbour, path + [neighbour]))
     return None
-
-class WordNode:
-    def __init__(self, word:str) -> None:
-        self.word = word
-        self.l1 = word[0]
-        self.l2 = word[1]
-        self.l3 = word[2]
-        self.neighbors = []
-
-
-    def get_neighbors(self):
-        return self.neighbors
-    
-class Graph:
-    def __init__(self) -> None:
-        self.set_of_wordnodes = set()
-
-    def add_word(self, new_wordnode) -> None:
-        self.set_of_wordnodes.add(new_word)
-        for wordnode in self.set_of_wordnodes:
-            if wordnode.l1 == new_wordnode.l1 or wordnode.l2 == new_wordnode.l2 or wordnode.l3 == new_wordnode.l3:
-                wordnode.add_neighbor(new_wordnode)
-                new_wordnode.add_neighbor(wordnode)
-    
-    def find_ladder(self, start, end):
